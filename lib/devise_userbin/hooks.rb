26423e961ddb14d0bf759553f37834b0a0267ff8
Johan Brissmyr <brissmyr@gmail.com>
2014-04-26 02:38:16 +0200

    Handle errors better in warden hooks

--- lib/devise_userbin/hooks.rb
+++ lib/devise_userbin/hooks.rb
@@ -4,18 +4,19 @@ Warden::Manager.after_set_user :except => :fetch do |record, warden, opts|
   begin
     session = Userbin::Session.post(
       "/api/v1/users/#{record.userbin_id}/sessions")
+    warden.session(opts[:scope])['_ubt'] = session.id
   rescue Userbin::Error
     # TODO: Handle challenges
   end
-
-  warden.session(opts[:scope])['_ubt'] = session.id
 end
 
 # Before logout
 #
 Warden::Manager.before_logout do |record, warden, opts|
   session_id = warden.session(opts[:scope]).delete('_ubt')
-  Userbin::Session.destroy_existing(session_id)
+  begin
+    Userbin::Session.destroy_existing(session_id)
+  rescue Userbin::Error; end
 end
 
 # Everytime current_<scope> is prepared
@@ -29,5 +30,5 @@ Warden::Manager.after_set_user :only => :fetch do |record, warden, opts|
     end
   rescue Userbin::SecurityError => error
     # TODO: Tampered or non-existing Userbin session. Log out
-  end
+  rescue Userbin::Error; end
 end
