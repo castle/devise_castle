0a0764a783344910c7b6bcb8542728dd14fa75fa
Johan Brissmyr <brissmyr@gmail.com>
2014-04-27 20:06:00 +0200

    Views generator

---
+++ lib/generators/devise_userbin/views_generator.rb
@@ -0,0 +1,19 @@
+require 'generators/devise/views_generator'
+
+module DeviseUserbin
+  module Generators
+    class ViewsGenerator < Rails::Generators::Base
+      desc 'Copies all DeviseUserbin views to your application.'
+
+      argument :scope, :required => false, :default => nil,
+                       :desc => "The scope to copy views to"
+
+      include ::Devise::Generators::ViewPathTemplates
+      source_root File.expand_path("../../../../app/views/devise", __FILE__)
+      def copy_views
+        view_directory :two_factor_authentication
+        view_directory :two_factor_recovery
+      end
+    end
+  end
+end
