1b3d8fa9d4ef385dd9ba4ff381e9bf26fff23ce9
Johan Brissmyr <brissmyr@gmail.com>
2014-05-03 22:04:47 +0200

    Remove model callbacks for now

--- lib/devise_userbin/model.rb
+++ lib/devise_userbin/model.rb
@@ -18,43 +18,6 @@
           false
         end
       end
-
-      included do
-        before_create :create_userbin_user
-        before_update :update_userbin_user
-        before_destroy :destroy_userbin_user
-
-        def create_userbin_user
-          userbin_user_block do
-            user = ::Userbin::User.create(email: email)
-            self.userbin_id = user.id
-          end
-        end
-
-        def update_userbin_user
-          if %w[email].any? { |attr| changed.include?(attr) }
-            userbin_user_block do
-              ::Userbin::User.save_existing(userbin_id, email: email)
-            end
-          end
-        end
-
-        def destroy_userbin_user
-          userbin_user_block do
-            ::Userbin::User.destroy_existing(userbin_id)
-          end
-        end
-
-        def userbin_user_block
-          begin
-            yield
-          rescue ::Userbin::Error => error
-            self.errors[:base] << error.to_s
-            false
-          end
-        end
-      end
-
     end
   end
 end
