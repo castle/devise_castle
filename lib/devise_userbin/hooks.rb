7049dcc4f8fc0b007868f7adc89ac4d3c4f61835
Johan Brissmyr <brissmyr@gmail.com>
2014-05-03 22:06:18 +0200

    Use new top-level API from userbin-ruby

--- lib/devise_userbin/hooks.rb
+++ lib/devise_userbin/hooks.rb
@@ -1,15 +1,23 @@
-# After login
+# Everytime current_<scope> is prepared
 #
-Warden::Manager.after_set_user :except => :fetch do |record, warden, opts|
+Warden::Manager.after_set_user :only => :fetch do |record, warden, opts|
+  scope = opts[:scope]
+
   begin
-    session = Userbin.with_context(warden.env) do
-      Userbin::Session.post("/api/v1/users/#{record.userbin_id}/sessions")
-    end
-    warden.session(opts[:scope])['_ubt'] = session.id
+    warden.session(scope)['_ubt'] = Userbin.authenticate({
+      user_id: record.id,
+      properties: {
+        email: record.email
+      },
+      current: warden.session(scope)['_ubt']
+    })
   rescue Userbin::ChallengeException => error
     warden.session(opts[:scope])['_ubc'] = error.challenge.id
-  rescue Userbin::Error
-    # TODO: Proceed silently or report to browser?
+  rescue Userbin::Error => error
+    warden.session(scope).delete('_ubt')
+    warden.session(scope).delete('_ubc')
+    warden.logout(scope)
+    throw :warden, :scope => scope, :message => :timeout
   end
 end
 
@@ -17,33 +25,7 @@ end
 #
 Warden::Manager.before_logout do |record, warden, opts|
   begin
-    if session_id = warden.session(opts[:scope]).delete('_ubt')
-      Userbin.with_context(warden.env) do
-        Userbin::Session.destroy_existing(session_id)
-      end
-    end
+    Userbin.deauthenticate(warden.session(opts[:scope]).delete('_ubt'))
   rescue Userbin::Error; end
 end
 
-# Everytime current_<scope> is prepared
-#
-Warden::Manager.after_set_user :only => :fetch do |record, warden, opts|
-  scope = opts[:scope]
-  session_id = warden.session(scope)['_ubt']
-
-  if session_id
-    begin
-      if Userbin::JWT.new(session_id).expired?
-        session = Userbin.with_context(warden.env) do
-          Userbin::Session.new(id: session_id).refresh
-        end
-        warden.session(scope)['_ubt'] = session.id
-      end
-    rescue Userbin::Error
-      warden.session(scope).delete('_ubt')
-      warden.session(scope).delete('_ubc')
-      warden.logout(scope)
-      throw :warden, :scope => scope, :message => :timeout
-    end
-  end
-end
