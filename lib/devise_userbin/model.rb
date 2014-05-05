1b99aa16125a2661bec5b6fba26bf1255c5cee69
Johan Brissmyr <brissmyr@gmail.com>
2014-05-05 20:51:36 +0200

    Remove user from Userbin on destroy

--- lib/devise_userbin/model.rb
+++ lib/devise_userbin/model.rb
@@ -5,13 +5,31 @@
 
       ::Userbin.api_secret = Devise.userbin_api_secret
 
+      included do
+        before_destroy :destroy_userbin_user
+
+        def destroy_userbin_user
+          userbin_user_block do
+            ::Userbin::User.destroy_existing(id)
+          end
+        end
+
+        def userbin_user_block
+          begin
+            yield
+          rescue ::Userbin::Error => error
+            self.errors[:base] << error.to_s
+            false
+          end
+        end
+      end
+
       # Overwrites valid_for_authentication? from Devise::Models::Authenticatable
       # for verifying whether a user is allowed to sign in or not.
       def valid_for_authentication?
         return super unless persisted?
 
         if super
-          # TODO: track successful login
           true
         else
           # TODO: track unsuccessful login
