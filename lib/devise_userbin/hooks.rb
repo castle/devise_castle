21d86c6fee43f2e5ba511807345cee5add9b70a2
Johan Brissmyr <brissmyr@gmail.com>
2014-05-12 11:25:24 +0200

    Move Userbin authentication to warden hook

--- lib/devise_userbin/hooks.rb
+++ lib/devise_userbin/hooks.rb
@@ -1,5 +1,34 @@
+# Everytime current_<scope> is prepared
+#
+Warden::Manager.after_set_user :only => :fetch do |record, warden, opts|
+  scope = opts[:scope]
+
+  begin
+    session_token = warden.request.session["#{scope}_userbin"]
+
+    session_token =
+      Userbin.authenticate(session_token, record.id, {
+        properties: {
+          email: record.email
+        },
+        context: {
+          ip: warden.request.ip,
+          user_agent: warden.request.user_agent
+        }
+      })
+
+    warden.request.session["#{scope}_userbin"] = session_token
+
+  rescue Userbin::Error => error
+    warden.logout(scope)
+    throw :warden, :scope => scope, :message => :timeout
+  end
+
+end
+
 Warden::Manager.before_logout do |record, warden, opts|
   begin
-    Userbin.deauthenticate(warden.request.session["#{opts[:scope]}_userbin"])
+    session_token = warden.request.session.delete("#{opts[:scope]}_userbin")
+    Userbin.deauthenticate(session_token)
   rescue Userbin::Error; end
 end
