fc329e7c9b0171beb12d91ecda95ad96f2d6e2c7
Johan Brissmyr <brissmyr@gmail.com>
2014-05-12 11:24:37 +0200

    Differentiate between notices and alerts

--- app/controllers/devise/devise_userbin_controller.rb
+++ app/controllers/devise/devise_userbin_controller.rb
@@ -9,29 +9,28 @@ class Devise::DeviseUserbinController < DeviseController
     render :show and return if params[:code].nil?
 
     Devise.mappings.keys.flatten.any? do |scope|
-      challenge_id = warden.session(scope)['_ubc']
-
       begin
         session["#{scope}_userbin"] =
           Userbin.verify_code(session["#{scope}_userbin"], params[:code])
-        set_flash_message :notice, :success if is_flashing_format?
+        set_flash_message :notice, :success
         redirect_to after_sign_in_path_for(scope)
       rescue Userbin::UserUnauthorizedError => error
-        set_flash_message :notice, :failed if is_flashing_format?
+        set_flash_message :alert, :failed
+        self.resource = resource_class.new
         respond_with_navigational(resource_name) { render :show }
       rescue Userbin::Forbidden => error
-        sign_out_with_message(:no_retries_remaining)
+        sign_out_with_message(:no_retries_remaining, :alert)
       rescue Userbin::Error => error
-        sign_out_with_message(:signed_out)
+        sign_out_with_message(:alert, :alert)
       end
     end
   end
 
   protected
 
-  def sign_out_with_message(message)
+  def sign_out_with_message(message, kind = :notice)
     signed_out = sign_out(resource_name)
-    set_flash_message :notice, message if signed_out && is_flashing_format?
+    set_flash_message kind, message if signed_out
     redirect_to after_sign_out_path_for(resource_name)
   end
 
