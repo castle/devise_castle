0710b3253011924f57e4e76bd419f76987536118
bartes <bartesrlz@gmail.com>
2018-11-27 09:55:05 +0100

    corrected getting user traits for password reset event

--- app/controllers/devise_castle/passwords_controller.rb
+++ app/controllers/devise_castle/passwords_controller.rb
@@ -1,14 +1,15 @@
 class DeviseCastle::PasswordsController < Devise::PasswordsController
   def create
     user_traits = Devise.reset_password_keys.each_with_object({}) do |key, acc|
-      acc[key] = resource_params[key] if resource_params.has?(key)
+      acc[key] = resource_params[key] if resource_params.has_key?(key)
     end
 
     super do |resource|
       unless resource.respond_to?(:castle_do_not_track?) && resource.castle_do_not_track?
         begin
           castle.track(
-            { event: '$password_reset.requested' }.merge({ user_traits: user_traits })
+            event: '$password_reset.requested',
+            user_traits: user_traits
           )
         rescue ::Castle::Error => e
           if Devise.castle_error_handler.is_a?(Proc)
@@ -26,11 +27,13 @@ class DeviseCastle::PasswordsController < Devise::PasswordsController
           if resource.errors.empty?
             castle.track(
               event: '$password_reset.succeeded',
-              user_id: resource._castle_id)
+              user_id: resource._castle_id
+            )
           else
             castle.track(
               event: '$password_reset.failed',
-              user_id: resource._castle_id)
+              user_id: resource._castle_id
+            )
           end
         rescue ::Castle::Error => e
           if Devise.castle_error_handler.is_a?(Proc)
